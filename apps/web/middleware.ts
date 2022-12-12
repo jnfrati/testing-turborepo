4ef6dc2074a17a69b2fad6eba660cd3ee3fd0cf6
jnfrati <nicofrati@gmail.com>
2022-12-12 09:53:14 -0300

    Precise matcher

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 const { BLOG_URL } = process.env as any;
 
 export const config = {
-  matcher: ["/:path*"],
+  matcher: ["/((?!api|_next|fonts|[\\w-]+\\.\\w+).*)"],
 };
 
 export default async function middleware(req: NextRequest) {
