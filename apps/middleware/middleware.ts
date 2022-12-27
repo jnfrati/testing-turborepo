39f976623a5082246902a19da773e75d123b6e20
jnfrati <nicofrati@gmail.com>
2022-12-27 16:33:09 -0300

    Apply same changes from admin to web rewriting

--- apps/middleware/middleware.ts
+++ apps/middleware/middleware.ts
@@ -19,11 +19,13 @@ export default async function middleware(req: NextRequest) {
     return NextResponse.rewrite(newUrl);
   }
 
+  const newUrl = new URL(process.env.ADMIN_URL || "");
+  newUrl.pathname = url.pathname;
   if (subdomain) {
-    url.pathname = `/${subdomain}${url.pathname}`;
+    newUrl.pathname = `/${subdomain}${newUrl.pathname}`;
   }
 
-  url.hostname = process.env.PUBLIC_URL || "";
-  console.log("REWRITE TO PUBLIC", url);
+  newUrl.hostname = process.env.PUBLIC_URL || "";
+  console.log("REWRITE TO PUBLIC", newUrl);
   return NextResponse.rewrite(url);
 }
